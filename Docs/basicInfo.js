299f860c490422a76adf74d432d261beb03e74e1
mrsl-yazilim <62723768+mrsl-yazilim@users.noreply.github.com>
2022-01-07 16:52:02 +0300

    Update basicInfo.js

--- Docs/basicInfo.js
+++ Docs/basicInfo.js
@@ -1,7 +1,7 @@
 module.exports = {
     openapi: "3.0.1",
     info: {
-        version: "2.0.0",
+        version: "0.1",
         title: "Whatsapp REST API",
         license: {
                 name: "MIT License",
